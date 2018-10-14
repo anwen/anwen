7caffeea2c05b493307297882e2a1142cf847871
askender <askender43@gmail.com>
2018-10-14 14:03:43 +0800

    add comment api

--- utils/avatar.py
+++ utils/avatar.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8-*-
-
+import hashlib
+import os
+import requests
 
 def get_avatar(email, size=16):
-    import hashlib
-    import os
-    import requests
     # gravatar_id = hashlib.md5(email.lower()).hexdigest()
     # size = str(size)
     # return "http://www.gravatar.com/avatar/%s?size=%s" % (gravatar_id, size)
-
     size = str(size)
     avatar_dir = 'static/avatar'
     default_avatar_path = '/%s/default/default.gif' % avatar_dir
@@ -16,7 +14,8 @@ def get_avatar(email, size=16):
     if os.path.isfile(avatar_path):
         return '/%s' % avatar_path
     else:
-        gravatar_id = hashlib.md5(email.lower()).hexdigest()
+        # gravatar_id = hashlib.md5(email.lower()).hexdigest() # raw
+        gravatar_id = hashlib.md5(email.lower().encode('u8')).hexdigest()
         link = "http://www.gravatar.com/avatar/%s?size=%s&d=404" % (
             gravatar_id, size)
         try:
