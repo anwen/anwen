cf1d2072b705c87cbe3e29860b784d1258f52300
askender <askender43@gmail.com>
2018-10-14 15:59:47 +0800

    fix api

--- utils/avatar.py
+++ utils/avatar.py
@@ -3,7 +3,19 @@ import hashlib
 import os
 import requests
 
-def get_avatar(email, size=16):
+
+def get_avatar(_id):
+    size = 'raw'
+    avatar_dir = 'static/avatar'
+    default_avatar_path = '/%s/default/default.gif' % avatar_dir
+    avatar_path = '%s/%s_%s.jpg' % (avatar_dir, str(_id), size)
+    if os.path.isfile(avatar_path):
+        return '/%s' % avatar_path
+    else:
+        return default_avatar_path
+
+
+def get_avatar_by_gravatar(email, size=16):
     # gravatar_id = hashlib.md5(email.lower()).hexdigest()
     # size = str(size)
     # return "http://www.gravatar.com/avatar/%s?size=%s" % (gravatar_id, size)
