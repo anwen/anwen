169dcd4bbb6bc8352ce7135484d3f37303abe71b
askender <askender43@gmail.com>
2018-10-21 18:29:25 +0800

    add script

--- anwen/base.py
+++ anwen/base.py
@@ -26,11 +26,11 @@ class BaseHandler(RequestHandler):
         return ns
 
     def set_default_headers(self):
-        print('set headers!!')
         self.set_header('Access-Control-Allow-Origin', '*')
         self.set_header('Access-Control-Allow-Headers', '*')
         self.set_header('Access-Control-Max-Age', 1000)
-        self.set_header('Content-type', 'application/json')
+        self.set_header('Content-type', 'text/html')
+        # self.set_header('Content-type', 'application/json')
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.set_header('Access-Control-Allow-Headers',
                         'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods')
