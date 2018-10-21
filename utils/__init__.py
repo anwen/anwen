9c2ef30672820222ff2baf3c48fc20f60154ff04
askender <askender43@gmail.com>
2018-10-21 17:13:54 +0800

    add script

--- utils/__init__.py
+++ utils/__init__.py
@@ -19,7 +19,7 @@ def make_emailverify():
     return str(time.time()) + str(uuid.uuid4())
 
 
-def send_email(receivers, subject, msg_body):
+def send_email_2(receivers, subject, msg_body):
     import options
     import smtplib
     from email.mime.text import MIMEText
@@ -42,6 +42,40 @@ def send_email(receivers, subject, msg_body):
     s.quit()
 
 
+def send_email(receivers, subject, msg_body):
+    import options
+    import smtplib
+    from email.mime.text import MIMEText
+    # from email.Header import Header
+    from email.header import Header
+    me = '%s<%s>' % (options.EMAIL_HOST_NICK, options.SERVICE_EMAIL)
+    host = options.EMAIL_HOST
+    port = options.EMAIL_PORT
+    user = options.EMAIL_HOST_USER
+    password = options.EMAIL_HOST_PASSWORD
+    msg = MIMEText(msg_body, 'html', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = me
+    msg['To'] = receivers
+    s = smtplib.SMTP()
+    s.set_debuglevel(1)  # show the debug log
+    s.connect(host, port)
+    # s.starttls()
+    s.login(user, password)
+    s.sendmail(me, receivers, msg.as_string())
+    s.quit()
+
+    # import smtplib
+    # from email.mime.text import MIMEText
+    # msg = MIMEText('hi')
+    # msg['Subject'] = 'The title'
+    # msg['From'] = 'me'
+    # msg['To'] = 'you'
+    # s = smtplib.SMTP('localhost')
+    # s.send_message(msg)
+    # s.quit()
+
+
 def random_sayings():
     file1 = 'utils/sayings.txt'
     a = []
@@ -50,6 +84,7 @@ def random_sayings():
             a.append(str(eachline.replace('\n', '')))
     import random
     return random.choice(a)
+
 
 if __name__ == '__main__':
     print(random_sayings())
