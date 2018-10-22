54503cf9441b82b1be9f1329be8f5f4f3553e02d
askender <askender43@gmail.com>
2018-10-22 21:38:11 +0800

    add script

--- devops/test_email.py
+++ devops/test_email.py
@@ -1,19 +1,9 @@
-import smtplib
-from email.mime.text import MIMEText
+import sys
+sys.path.append('.')
+import options
+import utils
 
-
-# fp = open(textfile, 'rb')
-# msg = MIMEText(fp.read())
-# fp.close()
-
-msg = MIMEText('hi')
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'The contents of %s' % textfile
-msg['From'] = 'me'
-msg['To'] = 'you'
-
-s = smtplib.SMTP('localhost')
-s.send_message(msg)
-s.quit()
+subject = 'test'
+msg_body = 'hi'
+email = options.superadmin_email
+utils.send_email(email, subject, msg_body)
