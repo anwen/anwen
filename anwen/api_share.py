87d1d29171bc9e94708cd2b5a4fc1a72fc64327a
shiji.lb <shiji.lb@alibaba-inc.com>
2018-10-23 22:15:58 +0800

    add script

--- anwen/api_share.py
+++ anwen/api_share.py
@@ -62,6 +62,11 @@ class ShareHandler(JsonHandler):  # 单篇文章
         #     share.url = '<a href="{}">{}</a>'.format(
         #         share.link, share.title)
         d_share = dict(share)
+        if d_share.get('link'):
+            doc = Webcache.find_one({'url': d_share['link']}, {'_id': 0})
+            if doc and doc['markdown']:
+                d_share['markdown'] += '\n--预览--\n' + doc['markdown']
+
         # thumbnails
         d_share['post_img'] = 'https://anwensf.com/static/upload/img/' + d_share['post_img'].replace('_1200.jpg', '_260.jpg')
 
