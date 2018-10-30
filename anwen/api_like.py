b953799b43fad8f030b9639ff05e0fcde015a266
askender <askender43@gmail.com>
2018-10-30 20:19:01 +0800

    add script

--- anwen/api_like.py
+++ anwen/api_like.py
@@ -8,20 +8,17 @@ class LikeHandler(JsonHandler):
 
     @tornado.web.authenticated
     def post(self, action):
-        # return self.post(action)
         entity_id = int(self.get_argument("entity_id", 0))
         entity_type = self.get_argument("entity_type", None)
         user_id = self.current_user["user_id"]
-        doc = {
-            'user_id': user_id,
-            'entity_id': entity_id,
-            'entity_type': entity_type,
-        }
-        newlikes = None
         assert action in 'addlike dellike adddislike deldislike'.split()
+
         _action = action[3:] + 'num'
         if entity_type == 'share':
             entity = Share.by_sid(entity_id)
+            # 如果是管理员，需要将status + 1
+            if user_id in (60, 63, 64) and action == 'addlike':
+                entity['status'] = entity['status'] + 1
         elif entity_type == 'comment':
             entity = Comment.by_sid(entity_id)
         elif entity_type == 'viewpoint':
@@ -30,12 +27,19 @@ class LikeHandler(JsonHandler):
             print('entity_type', entity_type, entity_id)
             return self.write_error(422, 'error params')
 
-        res = Like.change_like(doc, _action, action[:3])
         if action[:3] == 'add':
             entity[_action] += 1
         else:
             entity[_action] = 0
         entity.save()
+
+        doc = {
+            'user_id': user_id,
+            'entity_id': entity_id,
+            'entity_type': entity_type,
+        }
+        Like.change_like(doc, _action, action[:3])
+
         self.res = {
             'likenum': entity.likenum,
             'dislikenum': entity.dislikenum,
