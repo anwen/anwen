0fcf94cde85fab7d91cf9d874de4d11b862dfbac
askender <askender43@gmail.com>
2018-11-03 01:06:05 +0800

    fix api

--- devops/fix_like.py
+++ devops/fix_like.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # encoding:utf-8
-import os
-import hashlib
-import requests
+# import os
+# import hashlib
+# import requests
 import sys
 from pymongo import MongoClient
 conn = MongoClient()
 sys.path.append('.')
-from db import User, Share, Comment, Hit, Tag, Feedback, Admin, Like
+# from db import User, Share, Comment, Hit, Tag, Feedback, Admin, Like
 import options
 
 # 对于like, 冗余储存
@@ -18,24 +18,26 @@ def fix_like():
     adb = conn.anwen
     adb.authenticate(options.db['username'], options.db['password'])
 
+    # 数据库修复，默认为0
     for i in adb.Comment_Col.find():
         if 'likenum' not in i:
             adb.Comment_Col.update({'_id': i['_id']}, {'$set': {'likenum': 0}})
             adb.Comment_Col.update({'_id': i['_id']}, {'$set': {'dislikenum': 0}})
             print('done')
 
-    # just fix once
-    if 1:
-        for i in adb.Like_Col.find():
-            if i['entity_type'] == 'comment':
-                print(i)
-                entity_id = i['entity_id']
-                d = adb.Comment_Col.find_one({'id': entity_id})
-                print(d)
-                adb.Comment_Col.update({'id': entity_id}, {'$inc': {'likenum': i['likenum']}})
-                d = adb.Comment_Col.find_one({'id': entity_id})
-                print(d)
-                print('==')
+    # 修正喜欢次数
+    for i in adb.Like_Col.find():
+        print(i['likenum'])
+
+    # for i in adb.Like_Col.find():
+    #     if i['entity_type'] == 'comment':
+    #         print(i)
+    #         entity_id = i['entity_id']
+    #         d = adb.Comment_Col.find_one({'id': entity_id})
+    #         print(d)
+    #         adb.Comment_Col.update({'id': entity_id}, {'$inc': {'likenum': i['likenum']}})
+    #         d = adb.Comment_Col.find_one({'id': entity_id})
+    #         print(d)
 
 
 if __name__ == '__main__':
