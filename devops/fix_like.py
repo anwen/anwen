c71f11491d3c755321597bfb0fd83bfafc7b79d8
askender <askender43@gmail.com>
2018-11-22 21:11:20 +0800

    add script

--- devops/fix_like.py
+++ devops/fix_like.py
@@ -58,7 +58,7 @@ def fix_like():
         # if i['status'] < 1:
         # adb.Share_Col.update({'_id': i['_id']}, {'$set': {'status': 0}})
         n = 0
-        for j in adb.Like_Col.find({'entity_type': 'share', 'entity_id': i['id']}):
+        for j in adb.Like_Col.find({'entity_type': 'share', 'entity_id': i['entity_id']}):
             if j['user_id'] in (1, 60, 63, 64, 65) and j['likenum'] > 0:
                 print(j['likenum'])
                 n += 1
