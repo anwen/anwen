f260f8fe7e6818727d410ee726ed6a53f081b25c
askender <askender43@gmail.com>
2018-11-23 20:37:51 +0800

    add script

--- fabfile.py
+++ fabfile.py
@@ -54,23 +54,23 @@ def backup(c):
     c = Connection('aw')
     """ backup data from aw mongo """
     with c.cd('/var/www/anwen/db'):
-        c.run('. ~/.zshrc')
-        # c.run('python3 db_in_out.py')
-        # c.run('python3 db_in_out.py -o')
+        c.run('. ~/.zshrc && python3 db_in_out.py -o')
         c.run('tar czf aw_yaml.tar.gz data')
     with c.cd('/var/www/anwen/docs/shares'):
         c.run('tar czf aw_md.tar.gz *.md')
     with c.cd('/var/www/anwen/static/upload/'):
         c.run('tar czf upload.tar.gz img')
-
+    print('download yaml:')
     with cd(os.path.join(os.getcwd(), 'db/')):
         c.get('/var/www/anwen/db/aw_yaml.tar.gz', 'aw_yaml.tar.gz')
         c.local('tar zxf aw_yaml.tar.gz')
         c.local('rm aw_yaml.tar.gz')
+    print('download md:')
     with cd(os.path.join(os.getcwd(), 'docs/shares/')):
         c.get('/var/www/anwen/docs/shares/aw_md.tar.gz', 'aw_md.tar.gz')
         c.local('tar zxf aw_md.tar.gz')
         c.local('rm aw_md.tar.gz')
+    print('download img:')
     with cd(os.path.join(os.getcwd(), 'static/upload/')):
         c.get('/var/www/anwen/static/upload/upload.tar.gz', 'upload.tar.gz')
         c.local('tar zxf upload.tar.gz img')
