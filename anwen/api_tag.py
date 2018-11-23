62a06a2e87b030020e7f0d336897aa5032d228ff
askender <askender43@gmail.com>
2018-11-23 13:26:17 +0800

    add script

--- anwen/api_tag.py
+++ anwen/api_tag.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 from .api_base import JsonHandler
 from utils import get_tags
+from db import Tag, Share
+import tornado
+import time
 
 
 class TagsHandler(JsonHandler):
@@ -9,3 +12,31 @@ class TagsHandler(JsonHandler):
         d_tags = get_tags()
         self.res = d_tags
         self.write_json()
+
+    @tornado.web.authenticated
+    def post(self):
+        share_id = self.get_argument("id", None)
+        tags = self.get_argument("tags", '')
+        # user_id = self.current_user["user_id"]
+        tags = tags.strip()
+
+        if share_id:
+            share = Share.by_sid(share_id)
+            if share and tags not in share.tags:
+                tags = share.tags + ' ' + tags
+                res = {
+                    'tags': tags,
+                    'updated': time.time(),
+                }
+
+                share.update(res)
+                share.save()
+
+                tags = tags.split(' ')
+                tags = list(set(tags))
+                for i in tags:
+                    doc = {
+                        'name': i,
+                        'share_ids': share.id
+                    }
+                    Tag.new(doc)
