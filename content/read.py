f0637fb1b0036a4de4d284368d3ebe475392c1c3
askender <askender43@gmail.com>
2019-03-18 23:13:46 +0800

    add opml

--- content/read.py
+++ content/read.py
@@ -10,26 +10,38 @@ opml_file_Reabble = 'Reabble-zh.opml'
 
 
 # meta
-outline = opml.parse(opml_file_kindle4rss)
-# outline = opml.parse(opml_file_Reabble)
-print(outline)
+# outline = opml.parse(opml_file_kindle4rss)
+outline = opml.parse(opml_file_Reabble)
 print(outline.title)
 # print(outline.ownerName)
 # print(outline.ownerEmail)
 
 
-def read(rss_url):
+def read(rss_url, title):
     # d = feedparser.parse('http://www.reddit.com/r/python/.rss')
-    d = feedparser.parse(rss_url)
-    print(d['feed']['title'])
-    print(d['feed']['link'])  # Resolves relative links
-    print(d.feed.subtitle)
-    print(d.version)
-    print(len(d['entries']))
-    for entry in d['entries']:
-        print(entry['title'])
-        url = 'https://anwensf.com/share_by_get?link='+entry['link']
-        print(url)
+    print('\n\n====<br>')
+    try:
+        d = feedparser.parse(rss_url)
+        title = d['feed']['title']
+        # return
+        # print(d['feed']['link'])  # Resolves relative links
+        # d.feed.description
+        # d.feed.subtitle
+        # d.version
+        meta = '{} 文章数：{} <br>'.format(title, len(d['entries']))
+        print(meta)
+        for entry in d['entries']:
+            if 'weixin.sogou.com' in entry['link']:
+                continue
+            if 'yiding.9.cn' in entry['link']:
+                continue
+            # guid content:encoded pubDate
+            title = entry['title']
+            url = 'https://anwensf.com/share_by_get?link='+entry['link']
+            print('<a href="{}">{}</a><br>'.format(url, title))
+    except:
+        meta = '{}  err <br>'.format(title)
+        print(meta)
 
 
 assert len(outline)
@@ -44,19 +56,20 @@ for one in outline:
         raise
     if not len(one):
         # 无二级分类
-        print(one.xmlUrl)
-        read(one.xmlUrl)
-        break
+
+        read(one.xmlUrl, one.title)
+        # break
         if one.htmlUrl != one.xmlUrl:
             print(one.htmlUrl)
             raise
     else:
         for line in one:
             assert not len(line)
-            print(line.text)
+            # print(line.text)
             if line.title != line.text:
                 print(line.title)
-                raise
-            print(line.xmlUrl)
+                # raise
+            # print(line.xmlUrl)
+            read(line.xmlUrl, line.title)
             # if line.htmlUrl != line.xmlUrl:
             #     print('line.htmlUrl', line.htmlUrl)
---
+++ content/seed.md
@@ -0,0 +1,22 @@
+科幻
+编辑
+
+
+收藏
+
+
+
+
+日志 - 主动阅读
+
+订阅号阅读太封闭和被动，重启rss这个没落的方案。
+同时使用feedly和安问作为阅读器，以下记录一些相关信息。
+
+打开尘封许久的feedly，太多来源已经消失了，那些网站也都消失了：
+Following 1402 sources with 396 inactive and 668 unreachable.  
+我会用技术手段继续清理和整理一下，并将订阅源共享出来。
+
+想发起一个活动，「OPML共享」召回那些正在老去的geek
+以前的类似活动<https://www.appinn.com/share-opml/>
+
+分享一个网址：FeedX – RSS全文订阅<https://feedx.net/>
