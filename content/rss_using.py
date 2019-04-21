d8cd979c32c9b32c831e909f67e62e2417c42551
shiji.lb <shiji.lb@alibaba-inc.com>
2019-04-21 17:09:56 +0800

    fix

---
+++ content/rss_using.py
@@ -0,0 +1,26 @@
+rss_url = 'http://www.qdaily.com/feed.xml'
+rss_hostname = 'qdaily'
+rss_name = '好奇心日报'
+
+rss_url = 'https://www.solidot.org/index.rss'
+rss_hostname = 'solidot'
+rss_name = 'Solidot'  # 奇客资讯（IT新闻）
+
+rss_url = 'https://www.zhihu.com/rss'
+rss_hostname = 'zhihu'
+rss_name = '知乎每日精选'
+
+# gfw
+rss_url = 'https://feedx.net/rss/huxiu.xml'
+rss_hostname = 'huxiu'
+rss_name = '虎嗅'
+
+# both summary and content
+rss_url = 'https://www.jiqizhixin.com/rss'
+rss_hostname = 'jiqizhixin'
+rss_name = '机器之心'
+
+# 暂时放弃
+rss_url = 'https://www.gcores.com/rss'
+rss_hostname = 'gcores'
+rss_name = '机核'
