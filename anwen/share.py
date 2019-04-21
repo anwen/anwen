00f13aa5b97a11b4d84aaad4d470637af69d40fd
shiji.lb <shiji.lb@alibaba-inc.com>
2019-04-21 15:49:07 +0800

    fix and add rss

--- anwen/share.py
+++ anwen/share.py
@@ -79,7 +79,8 @@ class OneShareHandler(BaseHandler):
                 md = md.replace('![image](/', '![image]({}/'.format(domain))
                 md = md.replace('\n* \n', '\n\n')
                 md = md.replace('\n*\n', '\n\n')
-                md = md.replace('>\n\n', '')
+                md = md.replace('>\n', '> ')
+                # md = md.replace('>\n\n', '')  # ???
                 while '\n\n\n' in md:
                     md = md.replace('\n\n\n', '\n\n')
                 share.content = markdown2.markdown(md)
