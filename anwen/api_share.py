6b6de6164d1f749a07177119e86a3f91486caa8b
askender <askender43@gmail.com>
2019-04-27 19:20:19 +0800

    fix

--- anwen/api_share.py
+++ anwen/api_share.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from .api_base import JsonHandler
 from db import Share, Like, Collect, Viewpoint, Hit, Webcache, User
+from bs4 import BeautifulSoup
 from random import randint
 import random
 import requests
@@ -163,12 +164,29 @@ class SharesHandler(JsonHandler):
             '_id', -1).limit(per_page).skip((page - 1) * per_page)
         # shares = [fix_share(share) for share in shares]
         new_shares = []
-        print(shares.count())
         for share in shares:
             share = fix_share(share)
             user = User.by_sid(share.user_id)
             share = dict(share)
             share['user_name'] = user.user_name
+            share['markdown'] = ''
+
+            soup = BeautifulSoup(share['content'])
+            # kill all script and style elements
+            for script in soup(["script", "style"]):
+                script.extract()    # rip it out
+
+            # get text
+            text = soup.get_text()
+
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+            # print(text)
+            share['content'] = text[:150]
 
             if user.user_email.endswith('@wechat'):
                 share['user_img'] = options.site_url+get_avatar_by_wechat(user._id)
