286f8460f4749aaf7388f8487e90bab4ff13c13b
shiji.lb <shiji.lb@alibaba-inc.com>
2019-04-27 18:36:43 +0800

    fix

---
+++ anwen/users.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+import time
+# import hashlib
+import markdown2
+# import tornado.escape
+# import tornado.auth
+# from tornado import gen
+# import options
+# import utils
+# import utils.douban_auth
+# from utils.avatar import get_avatar
+# , CommonResourceHandler
+from .base import BaseHandler
+# , Share, Like
+from db import User, Share
+
+# 用户展示
+# 发了多少文章
+# 头像 user_name user_say user_lang user_tags
+
+
+class UsersHandler(BaseHandler):
+
+    def get(self):
+        users = User.find()
+        l_users = []
+        for user in users:
+            # print(user)
+            # user.user_say = markdown2.markdown(user.user_say)
+            # user.user_jointime = time.strftime(
+            #     '%Y-%m-%d %H:%M:%S', time.localtime(user.user_jointime))
+            # likenum = User.find({'user_id': user._id}).count()
+            # user.gravatar = get_avatar(user.user_email, 100)
+            contents = Share.find({'user_id': user.id})
+            if contents.count():
+                auser = {}
+                auser['user_domain'] = user.user_domain
+                auser['user_name'] = user.user_name
+                auser['article_num'] = contents.count()
+                l_users.append(auser)
+
+        self.render('users.html', users=l_users)
