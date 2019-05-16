ac87189c60d3895340f9ab52eba20411e6d1e295
shiji.lb <shiji.lb@alibaba-inc.com>
2019-05-16 21:18:03 +0800

    fix

--- content/rss_add_share.py
+++ content/rss_add_share.py
@@ -187,4 +187,4 @@ if __name__ == '__main__':
             if not key or (key and key == host):
                 add_from_file(url, host, name)
         print('start sleep for 3600s')
-        time.sleep(3600)
+        time.sleep(3600*4)
