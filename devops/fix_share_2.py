5d19fdb2f778f219cc20b34247c4ad6f2328f3af
askender <askender43@gmail.com>
2019-05-19 14:24:32 +0800

    fix

--- devops/fix_share_2.py
+++ devops/fix_share_2.py
@@ -19,11 +19,12 @@ def fix_share():
     adb2 = conn.anwen2
     adb2.authenticate(options.db['username'], options.db['password'])
     # for i in adb.Share_Col.find():
-    for idx in range(5694):
+    # for idx in range(5694):
+    for idx in range(1364, 5694):
         doc = adb.Share_Col.find_one({'id': idx})
         if doc:
             print(doc['id'])
-            adb2.Share_Col.insert(doc)
+            adb2.Share_Col_2.insert(doc)
         else:
             print('error: {}'.format(idx))
 
