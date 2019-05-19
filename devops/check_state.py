a46c802d21b69c9a940fb6bf619f17608861dc40
askender <askender43@gmail.com>
2019-05-19 15:10:35 +0800

    fix

--- devops/check_state.py
+++ devops/check_state.py
@@ -16,11 +16,56 @@ def check():
         print(collection)
 
 
+# User_Col
+# Share_Col
+
+# Like_Col
+# Hit_Col
+# Collect_Col
+
+# Comment_Col
+# Viewpoint_Col
+# Tag_Col
+# Admin_Col
+# Feedback_Col
+
+# Webcache_Col
+
 def check2():
+    # print(n, n2)
     n = adb.User_Col.find().count()
     n2 = adb.User_Col.find().sort('_id', -1)[0]['id']
-    print(n)
-    print(n2)
+    assert n == n2
+    n = adb.Share_Col.find().count()
+    n2 = adb.Share_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Like_Col.find().count()
+    n2 = adb.Like_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Hit_Col.find().count()
+    n2 = adb.Hit_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Collect_Col.find().count()
+    n2 = adb.Collect_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Comment_Col.find().count()
+    n2 = adb.Comment_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Viewpoint_Col.find().count()
+    n2 = adb.Viewpoint_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Tag_Col.find().count()
+    n2 = adb.Tag_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Admin_Col.find().count()
+    n2 = adb.Admin_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Feedback_Col.find().count()
+    n2 = adb.Feedback_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
+    n = adb.Webcache_Col.find().count()
+    n2 = adb.Webcache_Col.find().sort('_id', -1)[0]['id']
+    assert n == n2
 
 
 # check()
