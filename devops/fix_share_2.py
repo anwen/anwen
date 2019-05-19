100f1d3f430327d7d0b18b0a5906479115332176
shiji.lb <shiji.lb@alibaba-inc.com>
2019-05-19 14:37:30 +0800

    fix

--- devops/fix_share_2.py
+++ devops/fix_share_2.py
@@ -13,7 +13,7 @@ conn = MongoClient()
 # from db import User, Share, Comment, Hit, Tag, Feedback, Admin, Like
 
 
-def fix_share():
+def fix_share_1():
     adb = conn.anwen
     adb.authenticate(options.db['username'], options.db['password'])
     adb2 = conn.anwen2
@@ -28,6 +28,32 @@ def fix_share():
             adb2.Share_Col_3.insert(doc)
         else:
             print('error: {}'.format(idx))
+
+
+def fix_share():
+    adb = conn.anwen
+    adb.authenticate(options.db['username'], options.db['password'])
+    adb2 = conn.anwen2
+    adb2.authenticate(options.db['username'], options.db['password'])
+    # for i in adb.Share_Col.find():
+    # for idx in range(5694):
+    # for idx in range(1364, 5694):
+
+    for idx in range(1, 1364):
+        doc = adb2.Share_Col_1.find_one({'id': idx})
+        assert doc
+        print(doc['id'])
+        adb2.Share_Col.insert(doc)
+    for idx in range(1364, 5278):
+        doc = adb2.Share_Col_2.find_one({'id': idx})
+        assert doc
+        print(doc['id'])
+        adb2.Share_Col.insert(doc)
+    for idx in range(5277, 5694):
+        doc = adb2.Share_Col_3.find_one({'id': idx})
+        assert doc
+        print(doc['id'])
+        adb2.Share_Col.insert(doc)
 
 
 if __name__ == '__main__':
