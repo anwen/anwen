4e6f018d61477fe38a8426d5df6f7e09af45cc1b
shiji.lb <shiji.lb@alibaba-inc.com>
2019-05-25 17:30:36 +0800

    fix

--- devops/fix_hit.py
+++ devops/fix_hit.py
@@ -12,9 +12,10 @@ adb.authenticate(options.db['username'], options.db['password'])
 
 def fix():
     n = 1
-    for i in adb.Hit_Col.find().sort('_id', 1):
+    for i in adb.Hit_Col_v2.find().sort('_id', 1):
         i['id'] = n
-        adb.Hit_Col_v2.insert(i)
+        i['hitnum'] = 1
+        adb.Hit_Col.insert(i)
         n += 1
 
 
