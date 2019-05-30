954dacf762137166a9bfcb4b3cff29b46a4a20f8
askender <askender43@gmail.com>
2019-05-30 21:09:19 +0800

    fix

--- content/rss_add_share.py
+++ content/rss_add_share.py
@@ -104,10 +104,9 @@ def add_from_file(rss_url, rss_hostname, rss_name):
             elif 'T' in post.published:
                 # 2019-05-24T15:05:50-04:00
                 post.published = post.published[:-6]
-                tz = post.published[-6:].replace(':', '')
-
+                # tz = post.published[-6:].replace(':', '')
                 published = datetime.strptime(post.published, "%Y-%m-%dT%H:%M:%S")
-                published = published.replace(tzinfo=FixedOffset(tz))
+                # published = published.replace(tzinfo=FixedOffset(tz))
             else:
                 published = datetime.strptime(post.published, "%Y-%m-%d %H:%M:%S %z")
             published = published.timestamp()
