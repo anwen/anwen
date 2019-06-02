7f60535640e5543d6db80fdb807125730da7d163
askender <askender43@gmail.com>
2019-06-02 17:09:44 +0800

    fix

--- devops/fix_index.py
+++ devops/fix_index.py
@@ -11,7 +11,11 @@ adb.authenticate(options.db['username'], options.db['password'])
 
 r = adb.Share_Col.ensure_index('id', 1)
 print(r)
-r = adb.Share_Col.ensure_index('id', -1)
+r = adb.Share_Col.ensure_index('id', -1)  # ???
+print(r)
+r = adb.Share_Col.ensure_index('tags', 1)
+print(r)
+r = adb.Share_Col.ensure_index('status', 1)
 print(r)
 r = adb.Share_Col.index_information()
 print(r.keys())
