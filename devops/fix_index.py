e6dc7c4e5c92d8ce6f4832535ba70760072b6e87
askender <askender43@gmail.com>
2019-06-02 17:06:30 +0800

    fix

---
+++ devops/fix_index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding:utf-8
+
+import options
+from pymongo import MongoClient
+conn = MongoClient()
+
+adb = conn.anwen
+adb.authenticate(options.db['username'], options.db['password'])
+
+
+r = adb.Share_Col.ensure_index('id', 1)
+print(r)
+r = adb.Share_Col.ensure_index('id', -1)
+print(r)
+r = adb.Share_Col.index_information()
+print(r)
