2fb3ec2405d448bb456da445fc50121561940980
askender <askender43@gmail.com>
2019-06-03 19:58:36 +0800

    fix

--- anwen/api_shares.py
+++ anwen/api_shares.py
@@ -171,9 +171,9 @@ class SharesHandler(JsonHandler):
                 # meta['parent_tags'].append(d_tags_parent[tag])
                 meta['parent_tags'] = d_tags_parents[tag]
 
-        logger.info('last_suggested time: {}'.format(last_suggested)
+        logger.info('last_suggested time: {}'.format(last_suggested))
         logger.info('new_shares 0 time: {}'.format(new_shares[0]['published']))
-        self.res=list(new_shares)
-        self.meta=meta
+        self.res = list(new_shares)
+        self.meta = meta
         # number=len(self.res)
         return self.write_json(number=number)
