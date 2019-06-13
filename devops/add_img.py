bb5002bf72d1386e7faaf4edc3df6d27e9d62467
askender <askender43@gmail.com>
2019-06-14 01:00:23 +0800

    add yaml

--- devops/add_img.py
+++ devops/add_img.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # encoding:utf-8
+from utils.img_tools import make_post_thumb
+from PIL import Image
+import requests
+import datetime
 import sys
 import options
+import os
 from pymongo import MongoClient
 conn = MongoClient()
 adb = conn.anwen
 adb.authenticate(options.db['username'], options.db['password'])
 sys.path.append('.')
-
 from db import Tag, Share  # noqa
 
 
@@ -34,4 +38,36 @@ def check():
             print('https://anwensf.com/share/{}'.format(i['id']))
 
 
-check()
+def add():
+    fname = 'https://pic.001all.com/Wallpaper/Desktop%20Wallpaper/Space/FTP/2560%20x%201440/Shiny%20Star%20Wallpapers%20HD%202560%20x%201440%20Pixels%20Resolution.jpg'
+    # 上传的图片不能超过2M
+    ext = fname.split('.')[-1]
+    assert ext in ['.jpg', '.jpeg', '.gif', '.png', '.bmp']
+    img_dir = 'static/upload/img'
+    now = datetime.datetime.now()
+    t = now.strftime('%Y%m%d_%H%M%S_%f')
+    img_name = '%s%s' % (t, ext)
+    img_path = '%s/%s' % (img_dir, img_name)
+    # body = ''
+    r = requests.get(fname, stream=True)
+    with open(img_path, 'wb') as image:
+        image.write(r.raw)
+        # r.raw
+        # for chunk in r.iter_content(chunk_size):
+        #     fd.write(chunk)
+
+    im = Image.open(img_path)
+    width, height = im.size
+    if width / height > 5 or height / width > 5:
+        os.remove(img_path)  # 判断比例 删除图片
+        print('请不要上传长宽比例过大的图片')
+    else:
+        # 创建1200x550 750x230 365x230缩略图
+        make_post_thumb(img_path, sizes=[
+            (1200, 550), (750, 230), (365, 230), (260, 160)
+        ])
+        print('done')
+
+
+# check()
+add()
