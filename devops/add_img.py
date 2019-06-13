1e7f58dc8b8a19d7079ba3bcb1f53b7cc0d4d425
askender <askender43@gmail.com>
2019-06-13 20:41:25 +0800

    add yaml

--- devops/add_img.py
+++ devops/add_img.py
@@ -15,9 +15,12 @@ def check():
     for i in adb.Share_Col.find().sort('_id', 1):
         if i['status'] < 1:
             continue
+        # i['post_img']
         # post_img
         # upload_img
-        print(i['post_img'])
+        upload_img = i['upload_img']
+        if upload_img:
+            print('upload_img', upload_img)
 
 
 check()
