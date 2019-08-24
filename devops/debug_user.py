f416298ac08b5d2f9665a69fc2a9ff0dcf1c3e35
askender <askender43@gmail.com>
2019-08-24 17:02:53 +0800

    fix

---
+++ devops/debug_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding:utf-8
+
+import options
+from pymongo import MongoClient
+conn = MongoClient()
+
+adb = conn.anwen
+adb.authenticate(options.db['username'], options.db['password'])
+
+
+r = adb.User_Col.find('user_leaf', {'$gt': 0}).count()
+print(r)
