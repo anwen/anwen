98b65b1160da9cabe044367da8bb2405c2eb55f1
askender <askender43@gmail.com>
2019-11-14 21:33:55 +0800

    fix

--- anwen/api_tag.py
+++ anwen/api_tag.py
@@ -26,13 +26,14 @@ class TagsV2Handler(JsonHandler):
 
             parents = d_tags_parents.get(self.res['name'], [])
             self.res['parents'] = parents
-            parent_res = get_tags_v2_by_name(parents[0])
-            brothers = []
-            for sub in parent_res:
-                sub.pop('subs')
-                brothers.append(sub)
-            self.res['brothers'] = brothers
-            self.res['articleNumber'] = Share.count_by_tag(self.res['name'])
+            if parents:
+                parent_res = get_tags_v2_by_name(parents[0])
+                brothers = []
+                for sub in parent_res:
+                    sub.pop('subs')
+                    brothers.append(sub)
+                self.res['brothers'] = brothers
+                self.res['articleNumber'] = Share.count_by_tag(self.res['name'])
             tag = Tag.by_name(self.res['name'])
             if tag:
                 self.res['id'] = tag['id']
