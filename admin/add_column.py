b1dc7748c8135c4324b2d4c0dd294fc670f018fc
shiji.lb <shiji.lb@alibaba-inc.com>
2019-11-30 16:31:17 +0800

    fix egs

--- admin/add_column.py
+++ admin/add_column.py
@@ -2,16 +2,25 @@ import options
 from pymongo import MongoClient
 conn = MongoClient()
 
+adb = conn.anwen
+if 'username' in options.db:
+    adb.authenticate(options.db['username'], options.db['password'])
+
 
 def fix():
-    # 增加 author
-    adb = conn.anwen
-    if 'username' in options.db:
-        adb.authenticate(options.db['username'], options.db['password'])
     for i in adb.Tag_Col.find():
-        adb.Tag_Col.update({'_id': i['_id']}, {'$set': {'likenum': 0}})
-        adb.Tag_Col.update({'_id': i['_id']}, {'$set': {'dislikenum': 0}})
+        if 'likenum' not in i:
+            adb.Tag_Col.update({'_id': i['_id']}, {'$set': {'likenum': 0}})
+            adb.Tag_Col.update({'_id': i['_id']}, {'$set': {'dislikenum': 0}})
+
+
+def fix2():
+    for i in adb.User_Col.find():
+        tags = i['User_tags']
+        if tags:
+            print(tags)
 
 
 if __name__ == '__main__':
     fix()
+    fix2()
