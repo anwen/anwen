2813360bc6337ea056a6e398834c8951a21dde43
shiji.lb <shiji.lb@alibaba-inc.com>
2019-11-30 14:30:36 +0800

    fix egs

--- admin/fix_index.py
+++ admin/fix_index.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 # encoding:utf-8
-
+import sys
 import options
 from pymongo import MongoClient
-conn = MongoClient()
 
+conn = MongoClient()
 adb = conn.anwen
 adb.authenticate(options.db['username'], options.db['password'])
 
 
-# r = adb.Share_Col.ensure_index('id', -1)  # ???
-# print(r)
-r = adb.Share_Col.ensure_index('id', 1)
-print(r)
-r = adb.Share_Col.ensure_index('tags', 1)
-print(r)
-r = adb.Share_Col.ensure_index('status', 1)
-print(r)
-r = adb.Share_Col.ensure_index('suggested', -1)
-print(r)
+def add_index():
+    print('add_index')
 
-r = adb.Share_Col.index_information()
-print(r.keys())
+    # r = adb.Share_Col.ensure_index('id', -1)  # ???
+    # print(r)
+    r = adb.Share_Col.ensure_index('id', 1)
+    print(r)
+    r = adb.Share_Col.ensure_index('tags', 1)
+    print(r)
+    r = adb.Share_Col.ensure_index('status', 1)
+    print(r)
+    r = adb.Share_Col.ensure_index('suggested', -1)
+    print(r)
+    r = adb.User_Col.ensure_index('user_leaf', -1)
+    print(r)
 
 
-r = adb.User_Col.ensure_index('user_leaf', -1)
-print(r)
+def show_index():
+    print('show_index')
+    r = adb.Share_Col.index_information()
+    print(r.keys())
+
+
+show_index()
+# add_index()
