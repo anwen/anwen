03baedd8c2e3bf268e4e68533833562aa4c00299
askender <askender43@gmail.com>
2019-11-30 14:31:24 +0800

    fix egs

--- admin/fix_index.py
+++ admin/fix_index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # encoding:utf-8
-import sys
+# import sys
 import options
 from pymongo import MongoClient
 
 conn = MongoClient()
 adb = conn.anwen
+print(options.db)
 adb.authenticate(options.db['username'], options.db['password'])
 
 
