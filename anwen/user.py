bc269566194a7b0eb8ba26e6e0e05cdeb3f8d3a8
askender <askender43@gmail.com>
2019-12-15 19:43:05 +0800

    fix

--- anwen/user.py
+++ anwen/user.py
@@ -30,13 +30,14 @@ class UserhomeHandler(BaseHandler):
         if self.current_user:
             user_id = self.current_user["user_id"]
             _likes = Like.find({'entity_type': 'share', 'user_id': user_id}, {
-                               '_id': 0, 'id': 1, 'likenum': 1, 'dislikenum': 1})
+                               '_id': 0, 'entity_id': 1, 'likenum': 1, 'dislikenum': 1})
             _likes = list(_likes)
             print(_likes[0])
-            likes = set(i.id for i in _likes if i.likenum > 0)
-            dislikes = set(i.id for i in _likes if hasattr(i, 'dislikenum') and i.dislikenum > 0)
-            collects = Collect.find({'entity_type': 'share', 'user_id': user_id}, {'_id': 0, 'id': 1, 'collectnum': 1})
-            collects = set(i.id for i in collects if hasattr(i, 'collectnum') and i.collectnum > 0)
+            likes = set(i.entity_id for i in _likes if i.likenum > 0)
+            dislikes = set(i.entity_id for i in _likes if i.dislikenum > 0)
+            collects = Collect.find({'entity_type': 'share', 'user_id': user_id},
+                                    {'_id': 0, 'entity_id': 1, 'collectnum': 1})
+            collects = set(i.entity_id for i in collects if i.collectnum > 0)
 
         l_share = []
         print(shares[0])
